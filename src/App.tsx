797f459046dff6b1463e5678134930d4ec2f67e2
Dmytro_Gunko <dimagunko99@gmail.com>
2026-05-20 19:07:00 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to React</h1>
+      <p>
+        This is JSX — it looks like HTML, but it's not quite the same. It has
+        its own rules!
+      </p>
+    </div>
+  );
+}
+
+export default App;
